19d067e0b8203081d21a50e61e7c46336c2d46c3
pixelchai <30553505+pixelchai@users.noreply.github.com>
2022-07-12 00:12:25 +0100

    rework/extend plugin functionality

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Simple mpv script to easily customize random playlist song selection.
 Usage
 -----
 
-Every file initially has a 'rating' of `1`. Using the upvote/downvote keybindings, the rating of the currently playing file can be multiplied by 1.1 or 0.9. In RBS mode (activated by another keybinding), a new file is randomly chosen (chance [rating]/[files in playlist]) after playback of any file is completed.
+Every file initially has a 'rating' of `0.5`. Using the upvote/downvote keybindings, the rating of the currently playing file can be increased by decreased by 0.5.
+Use the shortcut Alt+r (by default) to advance to a new file from the rated files, randomly selected weighted on its rating.
 
 Installation and configuration
 ------------------------------
@@ -20,22 +21,4 @@ directory=/home/username/Music/BigPlaylist
 ratings_file=/home/username/.config/mpv/rbs-ratings.txt
 ```
 
-Set up the keybindings in your `input.conf` (usually located in `~/.config/mpv`):
-
-```
-Alt+r script-message RBS-init
-Alt+UP script-message RBS-upvote
-Alt+DOWN script-message RBS-downvote
-```
-
-Technical details
------------------
-
-Only absolute paths are stored in the `ratings_file`:
-
-```json
-{
-	"/home/username/Music/BigPlaylist/Topic/Directory/Song Name.mp3":1.100000,
-	"/home/username/Music/BigPlaylist/Another topic/LoremIpsum.mp3":0.810000
-}
-```
+Set up the keybindings in your `input.conf` (usually located in `~/.config/mpv`) as per the example in `input.conf`
--- ratings-based-shuffle.lua
+++ ratings-based-shuffle.lua
@@ -1,5 +1,8 @@
 utils = require 'mp.utils'
 require 'mp.options'
+require 'os'
+require 'io'
+require 'string'
 
 local options = {
     directory = ".",
@@ -10,26 +13,17 @@ read_options(options, "ratings-based-shuffle")
 all_files = {}
 ratings = {}
 
-function init_playlist()
-    mp.osd_message("Initializing ratings-based shuffle..")
-    mp.set_property("shuffle", "no")
-    mp.set_property("loop-playlist", "no")
-
-    load_ratings(options.ratings_file)
-    load(options.directory)
-
-    mp.commandv('loadfile', all_files[math.random(#all_files)], 'replace')
-
-    mp.register_event("end-file", auto_add_file)
-    auto_add_file(nil)
-    auto_add_file(nil)
+function init_rbs()
+    auto_add_file()
 end
 
 function load(path)
     for idx, name in ipairs(utils.readdir(path, "dirs")) do
+        print(path .. "/" .. name)
         load(utils.join_path(path, name))
     end
     for idx, name in ipairs(utils.readdir(path, "files")) do
+        print(path .. "/" .. name)
         table.insert(all_files, utils.join_path(path, name))
     end
 end
@@ -63,40 +57,82 @@ end
 
 function upvote()
     file = mp.get_property("path")
+    print(file)
     if ratings[file] == nil then
-        ratings[file] = 1.1
+        ratings[file] = 0.5
     else
-        ratings[file] = ratings[file] * 1.1
+        ratings[file] = ratings[file] + 0.5
     end
+    mp.osd_message("Rating: " .. tostring(ratings[file]))
     save_ratings(options.ratings_file)
 end
 
 function downvote()
     file = mp.get_property("path")
     if ratings[file] == nil then
-        ratings[file] = 0.9
+        ratings[file] = 0.0
     else
-        ratings[file] = ratings[file] * 0.9
+        ratings[file] = ratings[file] - 0.5
     end
+    mp.osd_message("Rating: " .. tostring(ratings[file]))
     save_ratings(options.ratings_file)
 end
 
+-- https://stackoverflow.com/q/2282444/5013267
+function table.contains(table, element)
+    for _, value in pairs(table) do
+      if value == element then
+        return true
+      end
+    end
+    return false
+end
+
 function auto_add_file(event)
-    while true do
-        idx = math.random(#all_files)
-        file = all_files[idx]
-        chance = ratings[file]
-        if chance == nil then
-            chance = 1.0
+    -- get all_files from ratings
+    for file, rating in pairs(ratings) do
+        if not table.contains(all_files, file) then
+            table.insert(all_files, file)
         end
-        if math.random() < (chance/#all_files) then
-            mp.commandv('loadfile', file, 'append')
+    end
+     
+    -- randomly add a file from all_files according to its rating (higher rating = more likely to be added)
+    -- if the file is already in the playlist, it will not be added again
+    -- a file in all_files has a corresponding rating value ratings[file]
+    print("auto_add_file")
+
+    -- return if the playlist is empty
+    if #all_files == 0 then
+        mp.osd_message("No files to add")
+        return
+    end
+    
+    -- weighted random selection
+    -- the sum of all ratings is the total weight of the selection
+    -- the selection is made by adding a random number between 0 and the total weight of the selection
+    totalSum = 0.0
+    for _, file in ipairs(all_files) do
+        totalSum = totalSum + ratings[file]
+    end
+    random = math.random() * totalSum
+    print("totalSum: " .. tostring(totalSum))
+    print("random: " .. tostring(random))
+    sum = 0.0
+    for _, file in ipairs(all_files) do
+        sum = sum + ratings[file]
+        print("sum: " .. tostring(sum))
+        if sum >= random then
+            print("adding file: " .. file)
+            mp.commandv("loadfile", file, "append")
+            mp.commandv("playlist-next")
+            mp.osd_message("Selected: " .. file .. "\nRating: " .. tostring(ratings[file]))
             break
         end
     end
 end
 
 math.randomseed(math.sin(os.time())*10000)
-mp.register_script_message("RBS-init", init_playlist)
+load_ratings(options.ratings_file)
+mp.register_script_message("RBS-init", init_rbs)
 mp.register_script_message("RBS-upvote", upvote)
 mp.register_script_message("RBS-downvote", downvote)
